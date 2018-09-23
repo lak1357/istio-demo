2042546c91c830f1a4caef0c5bcdbaa080f274e3
Lakshitha Herath (543830003) <lakshitha.herath@moneta.cz>
2018-09-23 13:12:42 +0530

    changes

--- src/main/java/com/mitrai/istio/controllers/DemoController.java
+++ src/main/java/com/mitrai/istio/controllers/DemoController.java
@@ -3,6 +3,7 @@ package com.mitrai.istio.controllers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
@@ -12,6 +13,19 @@ public class DemoController {
 
 	@Value("${SERVICE_NAME:istio-demo}")
 	private String service_name;
+
+	@RequestMapping("/hello")
+	public String hello() {
+
+		Long start = System.currentTimeMillis();
+
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(upstream_uri, String.class);
+
+		Long end = System.currentTimeMillis();
+
+		return service_name + " : " + (end - start) + "ms" + "\n" + upstream_uri + " : " + result;
+	}
 
 	@RequestMapping("/variables")
 	public String variables() {
